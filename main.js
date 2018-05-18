4d300c13b97254c93d24bfc8d040398eae57e74c
dustinpfister <dustin.pfister@gmail.com>
2018-05-18 17:45:39 -0400

    plane

--- main.js
+++ main.js
@@ -3,15 +3,35 @@ var scene = new THREE.Scene();
 
 // WHEEL
 var wheel = new HamsterWheel();
+wheel.group.position.set(0, 0, 1);
 scene.add(wheel.group);
 
 // GUY
-
 var guy = new Guy();
 guy.group.scale.set(.5, .5, .5);
-guy.group.position.set(0,  - .4, -1);
+guy.group.position.set(0,  - .4, 0);
 guy.group.rotation.set(0, Math.PI / 2, 0)
 scene.add(guy.group);
+
+var plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(50, 50, 10, 10),
+        [
+            new THREE.MeshStandardMaterial({
+                color: 0x00ffff
+            }),
+            new THREE.MeshStandardMaterial({
+                color: 0x00afaf
+            })
+
+        ]);
+plane.rotation.x = -Math.PI / 2;
+plane.position.y = -2.9;
+plane.geometry.faces.forEach(function (face, i) {
+
+    face.materialIndex = i % 2;
+
+});
+scene.add(plane);
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(50, 8 / 6, .05, 100);
@@ -22,7 +42,6 @@ scene.add(camera);
 
 var controls = new THREE.OrbitControls(camera);
 controls.autoRotate = true;
-
 
 // RENDER
 var renderer = new THREE.WebGLRenderer();
@@ -43,7 +62,7 @@ var loop = function () {
     guy.moveArm('arm_right',  - .1 + .2 * bias, 0);
     guy.moveArm('arm_left', .1 - .2 * bias, 0);
 
-	controls.update();
+    controls.update();
     renderer.render(scene, camera);
 
     frame += 1;
